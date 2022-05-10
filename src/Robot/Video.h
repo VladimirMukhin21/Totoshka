e22e1eeddc2591d5006c8ca8175191591bad0465
VladimirMukhin21 <vl.mukhin21@gmail.com>
2022-05-10 23:10:07 +0300

    Исправлен пин и изменен шим переключателя.

--- src/Robot/Video.h
+++ src/Robot/Video.h
@@ -21,17 +21,21 @@ void Video::init(byte switcherPin, byte frontServoPin, byte topServoPin) {
   _switcherPin = switcherPin;
   _front.init(frontServoPin, 0, 0, 180, 25);
   _top.init(topServoPin, 0, 0, 180, 25);
-  pinMode(switcherPin, OUTPUT);
+  pinMode(_switcherPin, OUTPUT);
 }
 
 void Video::setActiveCamera(byte activeCam) {
+  if (_activeCam == activeCam) {
+    return;
+  }
+
   _activeCam = activeCam;
-  switch (activeCam) {
+  switch (_activeCam) {
     case 0:
       analogWrite(_switcherPin, 0);
       break;
     case 1:
-      analogWrite(_switcherPin, 255);
+      analogWrite(_switcherPin, 127);
       break;
   }
 }
