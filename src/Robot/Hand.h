70669393e292d851d5811f8a5b5086a93e70e89f
Vladimir <vl.mukhin21@gmail.com>
2023-08-10 22:17:14 +0300

    Изменены настройки для езды по линии.

--- src/Robot/Hand.h
+++ src/Robot/Hand.h
@@ -35,7 +35,7 @@ private:
   Angle _shoulderAngle = Angle(30, 30, 154, 100);  // (115, 30, 154, 100)
   Angle _elbowAngle = Angle(0, 0, 120, 100);
   Angle _rotateAngle = Angle(_rotateCenterPos, 0, 180, 30);  // (3, 0, 180, 30)
-  Angle _clawAngle = Angle(97, 58, 180, 50);                 // (58, 58, 180, 50)
+  Angle _clawAngle = Angle(97, 58, 165, 50);                 // (58, 58, 180, 50)
 
   /*#ifdef DEBUG
         unsigned long _printTime = millis();
@@ -136,6 +136,34 @@ void Hand::tick() {
   _elbowAngle.setMinMax(0, constrain(0.75 * _shoulderAngle.toDeg() + 82.5, _elbowUpHandPos, 120));
 
 
+  if ((_handRideTheLineMode) && (millis() - _tickTime >= 1)) {
+    _tickTime = millis();
+
+    int shoulderPointsPre = _shoulderAngle.toPoints();
+    int elbowPointsPre = _elbowAngle.toPoints();
+    int rotatePointsPre = _rotateAngle.toPoints();
+    int clawPointsPre = _clawAngle.toPoints();
+
+    _shoulderAngle.addPoints(constrain(_shoulderRideTheLinePos - _shoulderAngle.toDeg(), -5, 5));
+    _shoulder.write(_shoulderAngle.toDeg());
+
+    _elbowAngle.addPoints(-8);
+    _elbow.write(_elbowAngle.toDeg());
+
+    _rotateAngle.addPoints(constrain(_rotateRideTheLinePos - _rotateAngle.toDeg(), -5, 5));
+    _rotate.write(_rotateAngle.toDeg());
+
+    _clawAngle.addPoints(-3);
+    _claw.write(_clawAngle.toDeg());
+
+    if (shoulderPointsPre == _shoulderAngle.toPoints()
+        && elbowPointsPre == _elbowAngle.toPoints()
+        && rotatePointsPre == _rotateAngle.toPoints()
+        && clawPointsPre == _clawAngle.toPoints()) {
+      _handRideTheLineMode = false;
+    }
+  }
+
 
   if ((_upHandMode) && (millis() - _tickTime >= 1)) {
     _tickTime = millis();
@@ -157,34 +185,6 @@ void Hand::tick() {
         && elbowPointsPre == _elbowAngle.toPoints()
         && rotatePointsPre == _rotateAngle.toPoints()) {
       _upHandMode = false;
-    }
-  }
-
-  if ((_handRideTheLineMode) && (millis() - _tickTime >= 1)) {
-    _tickTime = millis();
-
-    int shoulderPointsPre = _shoulderAngle.toPoints();
-    int elbowPointsPre = _elbowAngle.toPoints();
-    int rotatePointsPre = _rotateAngle.toPoints();
-    int clawPointsPre = _clawAngle.toPoints();
-
-    _shoulderAngle.addPoints(constrain(_shoulderRideTheLinePos - _shoulderAngle.toDeg(), -5, 5));
-    _shoulder.write(_shoulderAngle.toDeg());
-
-    _elbowAngle.addPoints(-5);
-    _elbow.write(_elbowAngle.toDeg());
-
-    _rotateAngle.addPoints(constrain(_rotateRideTheLinePos  - _rotateAngle.toDeg(), -5, 5));
-    _rotate.write(_rotateAngle.toDeg());
-
-    _clawAngle.addPoints(-5);
-    _claw.write(_elbowAngle.toDeg());
-
-    if (shoulderPointsPre == _shoulderAngle.toPoints()
-        && elbowPointsPre == _elbowAngle.toPoints()
-        && rotatePointsPre == _rotateAngle.toPoints()
-        && clawPointsPre == _clawAngle.toPoints()) {
-      _handRideTheLineMode = false;
     }
   }
 }
