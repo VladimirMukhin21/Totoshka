565c3f3713490368467c91753fe81fb9a651359a
VladimirMukhin21 <vl.mukhin21@gmail.com>
2023-11-19 13:26:20 +0300

    В программе захвата маяка подъезд теперь выполняется по гироскопу.

--- src/Robot/ProgTakeTin.h
+++ src/Robot/ProgTakeTin.h
@@ -5,33 +5,33 @@
 #include "DistMeter.h"
 
 class ProgTakeTin {
-  public:
-    void init(Truck &truck, Hand &hand, DistMeter &distMeter);
-    void start();
-    void stop();
-    void tick();
-    bool isRunning();
+public:
+  void init(Truck &truck, Hand &hand, DistMeter &distMeter);
+  void start();
+  void stop();
+  void tick();
+  bool isRunning();
 
-  private:
-    enum Phase {
-      NONE,
-      START,
-      HAND_DOWN,
-      DRIVE,
-      TAKE,
-      HAND_UP
-    };
+private:
+  enum Phase {
+    NONE,
+    START,
+    HAND_DOWN,
+    DRIVE,
+    TAKE,
+    HAND_UP
+  };
 
-    const int _driveSpeed = 50;
-    const int _distToTake = 70;
-    //GyverPID _pid = GyverPID(3.6, 0.15, 0.2, 100);
+  const int _driveSpeed = 70;
+  const int _distToTake = 70;
+  //GyverPID _pid = GyverPID(3.6, 0.15, 0.2, 100);
 
-    Truck* _truck;
-    Hand* _hand;
-    DistMeter* _distMeter;
-    Phase _phase = NONE;
+  Truck *_truck;
+  Hand *_hand;
+  DistMeter *_distMeter;
+  Phase _phase = NONE;
 
-    unsigned long _tickTime = millis();
+  unsigned long _tickTime = millis();
 };
 
 void ProgTakeTin::init(Truck &truck, Hand &hand, DistMeter &distMeter) {
@@ -69,33 +69,28 @@ void ProgTakeTin::tick() {
     _hand->handToTakeTin();
     _phase = HAND_DOWN;
     //Serial.println("start");
-  }
-  else if (_phase == HAND_DOWN) {
+  } else if (_phase == HAND_DOWN) {
     // ждем пока рука опустится...
     //Serial.println("init hand");
     if (!_hand->isRunning()) {
       // рука опустилась => подъезжаем к маяку
       _distMeter->enable();
+      _truck->goStraight(_driveSpeed);
       _phase = DRIVE;
     }
-  }
-  else if (_phase == DRIVE) {
+  } else if (_phase == DRIVE) {
     // подъезжаем к маяку
     int dist = _distMeter->getDist();
     //Serial.print("drive ");
     //Serial.println(dist);
-    if (dist > _distToTake) {
-      _truck->goAndTurn(_driveSpeed, 0);
-    }
-    else {
+    if (dist <= _distToTake) {
       // доехали до маяка
-      _truck->goAndTurn(0, 0);
+      _truck->stop();
       _distMeter->disable();
       _hand->takeTin();
       _phase = TAKE;
     }
-  }
-  else if (_phase == TAKE) {
+  } else if (_phase == TAKE) {
     // ждем пока рука сожмется...
     //Serial.println("take");
     if (!_hand->isRunning()) {
@@ -103,8 +98,7 @@ void ProgTakeTin::tick() {
       _hand->handUp();
       _phase = HAND_UP;
     }
-  }
-  else if (_phase == HAND_UP) {
+  } else if (_phase == HAND_UP) {
     // ждем пока рука с маяком поднимается...
     //Serial.println("hand up");
     if (!_hand->isRunning()) {
